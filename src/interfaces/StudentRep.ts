529cccc362f16d3d4287719aed4ac40d29d32484
Mihai Stancu <mihaistancu@mail.com>
2019-10-19 11:54:03 +0300

    Adds student group

--- src/interfaces/StudentRep.ts
+++ src/interfaces/StudentRep.ts
@@ -3,6 +3,7 @@ import { Subject } from "./Subject";
 import { TimeFrame } from "./TimeFrame";
 
 export interface StudentRep extends User {
+    group: string;
     subjects: Subject[];
     booked: TimeFrame[];
 }
