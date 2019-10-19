ccc53c769feb0e4b6b30be3b6424396b28f76f0c
Mihai <mihaistancu@mail.com>
2019-10-19 20:45:23 +0300

    Updates interfaces

--- src/interfaces/Needs.ts
+++ src/interfaces/Needs.ts
@@ -2,6 +2,8 @@ import * as db from "../others/db";
 
 export interface Needs extends db.DbObjectId {
     amphitheater: boolean;
-    laboratory: boolean;
+    blackboard: boolean; 
     computers: boolean;
+    laboratory: boolean;
+    projector: boolean;
 }
--- src/interfaces/Room.ts
+++ src/interfaces/Room.ts
@@ -3,6 +3,9 @@ import { Needs } from "./Needs";
 import { TimeFrame } from "./TimeFrame";
 
 export interface Room extends db.DbObjectId {
+    name: string;
     needs: Needs;
+    location: string;
+    capacity: number;    
     booked: TimeFrame[];
 }
